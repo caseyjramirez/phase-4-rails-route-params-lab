a7c67b35d782a89e216528767f1043519eaf16d5
caseyjramirez <caseyjramirez5@gmail.com>
2022-10-13 12:48:32 -0500

    commit tree d95dd87b662623399be4e1c98f417e7348eab11e

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,15 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = Student.where(first_name: params[:name] || last_name: params[:name])
+    # render json: students
+
     render json: students
+  end
+
+  def get_student
+    student = Student.find_by_id(params[:id])
+    render json: student
   end
 
 end
